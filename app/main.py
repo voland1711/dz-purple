ff23daec70adc5fa87ba2cca1b00a60f6f98e003
Your Name <you@example.com>
2026-08-31 14:01:52 +0500

    Выполнил ДЗ №4

--- app/main.py
+++ app/main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from posts import routes as post_routes
 from rand import routes as rand_routes
 
-app = FastAPI()
+app = FastAPI(
+    title="My Posts API",
+    description="API аналог приложения Twitter",
+    version="0.0.1",
+    openapi_tags=[
+        {"name": "Posts-service", "description": "Сервис для управления постами"}
+    ],
+)
 
 app.include_router(post_routes.router)
 app.include_router(rand_routes.router)
